60bf7270a9facaa55443807b1f070847b7afa80a
Andrea Debernardi <andrea@debbaweb.it>
2022-08-03 12:12:04 +0200

    open file and edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {createContext, useEffect, useMemo, useState} from 'react';
 import './App.css';
+import SelectFile from "./SelectFile";
+import EditFile from "./EditFile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [fileAccessApiAvailable, setFileAccessApiAvailable] = useState<boolean>(true);
+    const [file, setFile] = useState<File | null>(null);
+
+    const FileContext = createContext<{ file: File | null, setFile: Function }>({
+        file: null,
+        setFile: () => {
+        }
+    });
+
+    const value = useMemo(
+        () => ({file, setFile}),
+        [file]
+    )
+
+    useEffect(() => {
+        if (!('showOpenFilePicker' in window)) {
+            setFileAccessApiAvailable(false);
+        }
+    }, [])
+
+
+    return (
+        <div className="App">
+            {
+                fileAccessApiAvailable ? (
+
+                    <FileContext.Provider value={value}>
+                        {
+                            value.file === null ?
+                                <SelectFile onSelectFile={value.setFile}/> :
+                                <EditFile onClose={() => value.setFile(null)} file={value.file}></EditFile>
+                        }
+                    </FileContext.Provider>
+                ) : (
+                    <div className="flex items-center space-x-6">
+                        <div className="flex-1">
+                            <p className="text-sm text-gray-600">
+                                File access API is not available
+                            </p>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/EditFile.tsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import {XCircleIcon} from "@heroicons/react/solid";
+
+const EditFile: React.FC<{ file: File, onClose: () => void }> = ({file, onClose}) => {
+
+    const [fileContent, setFileContent] = useState<string>('');
+
+    useEffect(() => {
+
+        file.text().then(
+            fileContent => setFileContent(fileContent)
+        );
+
+    }, [file]);
+
+
+    const closeOpenedFile = () => {
+        if (window.confirm(`Are you sure you want to close the file ${file.name}?`)) {
+            onClose();
+        }
+    };
+
+    const saveFile = () => {
+        alert('SAVE FILE!');
+    };
+
+    return (
+        <>
+            <div className="bg-gray-900">
+                <div className="px-5 mx-auto h-10 flex items-center justify-between">
+                    <div className="flex-1">
+                        <XCircleIcon onClick={() => closeOpenedFile()} fill="red"
+                                     className="-ml-1 mr-2 h-5 w-5 cursor-pointer" aria-hidden="true"/>
+                    </div>
+                    <div className="flex-shrink-0">
+                        <h3 className="text-lg leading-6 font-medium text-white">{file.name}</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="mx-2">
+
+                <div className="grid grid-cols-1 gap-6">
+                <textarea
+                    rows={10}
+                    onChange={e => setFileContent(e.target.value)}
+                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                    value={fileContent}
+                />
+                </div>
+                <div className="text-right my-2">
+                    <button
+                        type="button"
+                        onClick={e => saveFile() }
+                        className="inline-flex items-center px-2.5 py-1.5 text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Save
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EditFile;
