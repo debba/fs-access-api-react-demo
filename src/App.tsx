48bf6838a29641916b6b22fdfb63c344225fe359
Andrea Debernardi <andrea@debbaweb.it>
2022-08-03 13:02:06 +0200

    template fix

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,14 @@ function App() {
     }, []);
 
     useEffect(() => {
-        if (file){
-            document.title = 'Current file opened: '+file.name;
+        if (file) {
+            document.title = 'Current file opened: ' + file.name;
         }
     }, [file]);
 
 
     return (
-        <div className="App">
+        <div className="App grid grid-cols-1 bg-gray-100 min-h-screen">
             {
                 fileAccessApiAvailable ? (
 
@@ -47,14 +47,28 @@ function App() {
                     <div className="flex items-center space-x-6 mt-2">
                         <div className="flex-1 px-2">
                             <p className="text-lg text-gray-600 text-center">
-                                File access API is not available in this browser.
+                                File system access API is not available in this browser.
+                            </p>
+                            <p className="text-center text-gray-600">
+                                You can check <strong>browser compatibility</strong> <a
+                                href="https://developer.mozilla.org/en-US/docs/Web/API/File_System_Access_API#browser_compatibility">
+                                here
+                            </a>
                             </p>
                         </div>
                     </div>
                 )
             }
 
+            <footer
+                className="mt-auto bg-white rounded-lg shadow md:flex md:items-end md:p-6">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://www.debbaweb.it/"
+                                                                                        className="hover:underline">Andrea Debernardi™</a>. All Rights Reserved.
+    </span>
+            </footer>
+
         </div>
+
     );
 }
 
